1e581f0a14449a585229f01cf37aa7cdda42d6d5
alex.zhou.hi.man <alex.zhou.hi.man@gmail.com>
2022-12-25 13:42:23 +0800

    chore: update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	Name          = "vmess-node"
-	Version       = "0.1.7"
+	Version       = "0.1.8"
 	CopyRight     = "XFLASH-PANDA@2021"
 	LogLevelDebug = "debug"
 	LogLevelError = "error"
--- internal/pkg/api/model.go
+++ internal/pkg/api/model.go
@@ -15,7 +15,7 @@ type UserTraffic struct {
 	UID      int   `json:"user_id"`
 	Upload   int64 `json:"u"`
 	Download int64 `json:"d"`
-	Count    int64 `json:"count"`
+	Count    int64 `json:"n"`
 }
 
 type RepUserTraffic struct {
