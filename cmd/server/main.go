9233e53882b1c16494f3e9712d12d4d303afb599
wwnice-max <wwnice-max@outlook.com>
2023-12-06 07:40:07 +0800

    chore: update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	Name          = "vmess-node"
-	Version       = "0.1.16"
+	Version       = "0.1.17"
 	CopyRight     = "XFLASH-PANDA@2021"
 	LogLevelDebug = "debug"
 	LogLevelError = "error"
